33335ce605e10813378c73ac0acdc8a902b49c2e
uiwwnw Yoon <uiwwnwyoon@uiwwnwui-MacBook.local>
2018-03-06 21:51:28 +0900

    마우스우클릭해제

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// console.log(Data);
 
 class App extends React.Component {
     constructor() {
@@ -13,6 +12,7 @@ class App extends React.Component {
         }
     }
     newstart() {
+        // 새로게임 시작하기
         const _this = this;
         this.setState({start:false})
         
@@ -20,6 +20,7 @@ class App extends React.Component {
         document.getElementById('findMine').classList.remove('win');
     }
     restart() {
+        //재시작하기
         const _this = this;
         this.setState({start:false})
         document.getElementById('findMine').classList.add('restart');
@@ -30,7 +31,25 @@ class App extends React.Component {
             document.getElementById('findMine').classList.remove('restart');
         }, 0)
     }
+    nextstart() {
+        //다음레벨시작하기
+        const _this = this;
+        this.setState({
+            start:false,
+            level: this.state.level-1<3?9:this.state.level-1,
+            x:this.state.level-1<3?this.state.x+1:this.state.x,
+            y:this.state.level-1<3?this.state.y+1:this.state.y
+        })
+        document.getElementById('findMine').classList.add('restart');
+        document.getElementById('findMine').classList.remove('lose');
+        document.getElementById('findMine').classList.remove('win');
+        setTimeout(function() {
+            _this.start();
+            document.getElementById('findMine').classList.remove('restart');
+        }, 0)
+    }
     start() {
+        //시작하기
         const date = new Date();
         this.setState({
             start:true,
@@ -39,6 +58,7 @@ class App extends React.Component {
         
     }
     changeLv(e) {
+        // 레벨변경
         let num = prompt('3~9 중 입력해주세요~ 높을수록 어렵습니다.');
         if(num > 9) {
             num = 9;
@@ -50,6 +70,7 @@ class App extends React.Component {
         })
     }
     changeHo(e) {
+        // 크기변경
         let num = prompt('7~31 중 홀수를 입력해주세요~');
         if(num%2===0) {
             num = num-1;
@@ -64,6 +85,7 @@ class App extends React.Component {
         })
     }
     changeVe(e) {
+        // 크기변경
         let num = prompt('7~31 중 홀수를 입력해주세요~');
         if(num%2===0) {
             num = num-1;
@@ -81,7 +103,7 @@ class App extends React.Component {
         let box = '';
         let pack = '';
         if(this.state.start) {
-            box = <Box x={this.state.x} y={this.state.y} level={this.state.level} newstart={this.newstart.bind(this)} restart={this.restart.bind(this)} startDate={this.state.startDate}/>;
+            box = <Box x={this.state.x} y={this.state.y} level={this.state.level} newstart={this.newstart.bind(this)} restart={this.restart.bind(this)} nextstart={this.nextstart.bind(this)} startDate={this.state.startDate}/>;
         } else {
             pack = 
             <div className="startPack">
@@ -112,9 +134,7 @@ class Box extends App {
         }
     }
     check() {
-        // console.log(this.props.startDate);
-        // console.log(this.state.bomb.length)
-        // console.log()
+        // 성공체크
         if(this.state.complete.length === 0 && this.state.bomb.length === Object.keys(this.state.maps).length){
             document.getElementById('findMine').classList.add('win');
             this.setState({
@@ -123,6 +143,7 @@ class Box extends App {
         };
     }
     markRemove(e) {
+        // 깃발,폭탄체크 제거
         this.check();
         const target = e.target.parentNode.parentNode.parentNode.childNodes[0];
         const id = target.id;
@@ -137,9 +158,9 @@ class Box extends App {
         (el.classList.contains('markFlag'))&&(el.classList.remove('markFlag'));
         (el.classList.contains('markBomb'))&&(el.classList.remove('markBomb'));
         el.innerHTML = '';
-        // delete this.state.maps[id]; 
     }
     markFlag(e) {
+        // 깃발체크
         const target = e.target.parentNode.parentNode.parentNode.childNodes[0];
         const id = target.id;
         if(target.classList[1] === 'active') {
@@ -152,29 +173,25 @@ class Box extends App {
             this.state.complete.push(id);
         }
         this.check();
-        // console.log(this.state.complete,this.state.complete.length);
     }
     markBomb(e) {
+        // 폭탄체크
         const target = e.target.parentNode.parentNode.parentNode.childNodes[0];
         const id = target.id;
-        // console.log(id);
         if(target.classList[1] === 'active') {
             return false;
         }
         const el = document.getElementById(id);
         const num = this.state.complete.indexOf(id);
         el.classList.add('markBomb');
-        // el.setAttribute('data-bomb-index', num);
         el.innerHTML = '<i class="icon-bomb"></i>';
         if(num !== -1) {
             this.state.complete.splice(num, 1);
         }
-        // console.log(this.state.complete,this.state.complete.length);
         this.check();
     }
     click(e) {
-        // const slt = [];
-        // console.log(e.target);
+        // 클릭
         if(e.target.classList[1] === 'active' || e.target.classList[1] === 'markBomb' || e.target.classList[1] === 'markFlag') {
             return false;
         }
@@ -183,56 +200,46 @@ class Box extends App {
         const id = e.target.id;
         const idx = this.state.maps[e.target.id] === undefined ? '' : this.state.maps[e.target.id];
         const el = document.getElementById(id);
-        // console.log(el)
         const bomb = idx === 'B' ? false : true;
         const xIdx = id.indexOf('x');
         const yIdx = id.indexOf('y');
         let pX = Number(id.substring(0, xIdx));
         let pY = Number(id.substring(xIdx + 1, yIdx));
         function rp(_pos) {
-            // console.log(_pX);
-            // let sto;
             if (_pos !== undefined) {
                 list.shift();
             }
             pX = _pos === undefined ? pX : _pos.x;
             pY = _pos === undefined ? pY : _pos.y;
-            // console.log(_pX,_pY);
-            // console.log(pX,pY);
             for (let j = -1; j < 2; j++) {
                 for (let k = -1; k < 2; k++) {
                     if (j === 0 && k === 0) {
                     } else {
                         if (Object.keys(_this.state.maps).indexOf((pX + j) + 'x' + (pY + k) + 'y') !== -1 && !document.getElementById((pX + j) + 'x' + (pY + k) + 'y').classList.contains('markBomb')&&!document.getElementById((pX + j) + 'x' + (pY + k) + 'y').classList.contains('markFlag')) {
-                            // console.log((pX + j) + 'x' + (pY + k) + 'y');
                             if (_this.state.maps[(pX + j) + 'x' + (pY + k) + 'y'] !== 'B') {
                                 document.getElementById((pX + j) + 'x' + (pY + k) + 'y').classList.add('active');
                                 document.getElementById((pX + j) + 'x' + (pY + k) + 'y').innerText = _this.state.maps[(pX + j) + 'x' + (pY + k) + 'y'] === undefined ? '' : _this.state.maps[(pX + j) + 'x' + (pY + k) + 'y'];
 
                                 if (_this.state.maps[(pX + j) + 'x' + (pY + k) + 'y'] === undefined) {
-                                    // console.log(_this.state.maps[(pX + j) + 'x' + (pY + k) + 'y']);
                                     let pos = {};
                                     pos.x = pX + j;
                                     pos.y = pY + k;
                                     list.push(pos);
                                 }
                                 delete _this.state.maps[(pX + j) + 'x' + (pY + k) + 'y'];
-                                // console.log(Object.keys(_this.state.maps).indexOf((pX + j) + 'x' + (pY + k) + 'y'))
-                                // console.log(Object.keys(_this.state.maps).length)
                             }
                         }
                     }
-                    // console.log();
                 }
             }
             if (list.length <= 0) { return false; } else {
                 for (let i = 0; i < list.length; i++) {
-                    // console.log(list);
                     new rp(list[i]);
                 }
             }
         }
         if (!bomb) {
+            // 폭탄 클릭 시
             this.setState({
                 end:'lose'
             });
@@ -249,10 +256,9 @@ class Box extends App {
             el.innerText = idx;
         }
         this.check();
-        // console.log(Object.keys(this.state.maps).length);
     }
     makeIdx() {
-        // let l = {};
+        // 임의 수 지정하기, 폭탄만들기, 폭탄주변 숫자 더하기
         for (let i = 0; i < Object.keys(this.state.maps).length; i++) {
             const p = Object.values(this.state.maps).indexOf('B', i);
             if(this.state.bomb.indexOf(Object.keys(this.state.maps)[p]) === -1 && Object.keys(this.state.maps)[p] !== undefined) {
@@ -263,31 +269,25 @@ class Box extends App {
 
         for (let i = 0; i < this.state.bomb.length; i++) {
             const id = this.state.bomb[i];
-            // console.log(el)
-            // console.log(id);
             const xIdx = id.indexOf('x');
             const yIdx = id.indexOf('y');
             const pX = Number(id.substring(0, xIdx));
             const pY = Number(id.substring(xIdx + 1, yIdx));
-            // console.log(id)
             for (let j = -1; j < 2; j++) {
                 for (let k = -1; k < 2; k++) {
                     if (Object.keys(this.state.maps).indexOf((pX + j) + 'x' + (pY + k) + 'y') !== -1 && this.state.maps[(pX + j) + 'x' + (pY + k) + 'y'] !== 'B') {
                         let num = (this.state.maps[(pX + j) + 'x' + (pY + k) + 'y'] === undefined) ? 0 : this.state.maps[(pX + j) + 'x' + (pY + k) + 'y'];
-                        // console.log(this.state.maps[(pX+j)+'x'+(pY+k)+'y']===undefined,(pX+j)+'x'+(pY+k)+'y')
                         this.state.maps[(pX + j) + 'x' + (pY + k) + 'y'] = num + 1;
                     }
                 }
             }
         }
-        // this.state.maps['-5x5y']='aaaa';
-        // console.log(Object.keys(l).length);
-        // console.log(this.state.maps);
     }
     marking(e) {
         e.target.parentNode.setAttribute('style','opacity:.3');
     }
     random(n) {
+        // 렌덤수
         let rd = Math.floor((Math.random() * n)) === 0 ? 'B' : undefined;
         return rd;
     }
@@ -301,6 +301,7 @@ class Box extends App {
                 <p>맵크기{this.props.x*this.props.y} 레벨 {this.props.level} 완료하는데 총 {Math.ceil((new Date() - this.props.startDate)/1000)}초걸렸어요.</p>
                 <p>{this.props.x*this.props.y/this.props.level*3 >Math.ceil((new Date() - this.props.startDate)/1000)?'정말 잘하시네요~~':'조금더분발해주세요~~'}</p>
                 <button onClick={this.props.restart.bind(this)}>다시시작하기</button>
+                <button onClick={this.props.nextstart.bind(this)}>다음레벨시작하기</button>
                 <button onClick={this.props.newstart.bind(this)}>새로시작하기</button>
             </div>;
         }else if (this.state.end === 'lose') {
@@ -314,17 +315,13 @@ class Box extends App {
         }
         const x = Number(this.props.x) + 1;
         const y = Number(this.props.y) + 1;
-        // console.log(Object.values(this.state.maps).length);
         for (let i = (y / 2 - 1); i > -(y / 2); i--) {
             for (let j = -(x / 2 - 1); j < (x / 2); j++) {
                 maps.push(j + 'x' + i + 'y');
                 this.state.maps[j + 'x' + i + 'y'] = this.random(this.props.level);
             }
         }
-        // console.log(this.state.ids.length);
-        // console.log(Object.values(this.state.maps).length);
         this.makeIdx();
-        // console.log(Object.values(this.state.maps).length);
         return (
             <div className="maps" data-x={this.props.x} data-y={this.props.y}>
                 {maps.map((item, i) => {
@@ -347,15 +344,18 @@ class I extends Box {
         }
     }
     out() {
+        // 마우스 아웃
         clearTimeout(this.state.sto);
     }
     up() {
+        // 마우스업
         clearTimeout(this.state.sto);
         this.setState({
             hold: false
         })
     }
     down() {
+        // 마우스 다운
         const end = (document.getElementById('findMine').classList.contains('lose')===true||document.getElementById('findMine').classList.contains('win')===true)?true:false;
         const _this = this;
         if(!end){
@@ -371,9 +371,9 @@ class I extends Box {
         if (this.state.hold) {
             popover =
                 <div className="popover">
-                    <span onMouseUp={this.props.markFlag.bind(this)} onTouchStart={this.props.markFlag.bind(this)}><i className="icon-flag"></i></span>
-                    <span onMouseUp={this.props.markBomb.bind(this)} onTouchStart={this.props.markBomb.bind(this)}><i className="icon-bomb"></i></span>
-                    <span onMouseUp={this.props.markRemove.bind(this)} onTouchStart={this.props.markRemove.bind(this)}><i className="icon-trash"></i></span>
+                    <span onMouseUp={this.props.markFlag.bind(this)} onTouchEnd={this.props.markFlag.bind(this)}><i className="icon-flag"></i></span>
+                    <span onMouseUp={this.props.markBomb.bind(this)} onTouchEnd={this.props.markBomb.bind(this)}><i className="icon-bomb"></i></span>
+                    <span onMouseUp={this.props.markRemove.bind(this)} onTouchEnd={this.props.markRemove.bind(this)}><i className="icon-trash"></i></span>
                 </div>
         }
         return (

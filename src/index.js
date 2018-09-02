61a6f785c31a2ba2c334e6bb2bbb18f89d54f87b
uiwwnw Yoon <uiwwnwyoon@uiwwnwui-MacBook.local>
2018-09-02 13:53:06 +0900

    delete

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import './style.scss';
-import React, { Component } from 'react';
-import {render} from 'react-dom';
-import {sortable} from 'react-sortable';
-import App from './components/App';
-render(<App />, document.getElementById('root'));
